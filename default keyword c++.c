bdc20c3c031eafdcbbddec8dce72420346423404
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 12:00:00 +0530

    Create default keyword c++.c

---
+++ default keyword c++.c
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int day = 4;
+    switch (day){
+    case 6:
+        cout << " today is monday";
+        break;
+    case 7:
+        cout << "today is what";
+        break;
+    default:
+        cout << "looking forword with the new life and new lifestyle";
+
+    }
+    return 0;
+}
